8702b6f0049b506b19b4614492d008f3249e7a81
Dmitry Buggenhout <dmitry.buggenhout@gmail.com>
2025-02-18 15:03:57 +0100

    clang-format
    Wrapper around clang-format

---
+++ trenchkit/tools/clang-format/main.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import argparse
+
+CONFIGURATION = {}
+
+def load_configuration():
+	with open(os.path.join(os.path.dirname(__file__), "settings.conf"), "r") as configuration_file:
+		lines = configuration_file.read().splitlines()
+		for line in lines:
+			key, value = line.split('=')
+			CONFIGURATION[key] = value
+
+def run(args):
+	parser = argparse.ArgumentParser(description="clang-format Program")
+	parser.add_argument("-path", help="Path for directory to format", default=".")
+	parser.add_argument("-v", action="store_true", help="Verbose mode")
+	parser.add_argument("args", nargs=argparse.REMAINDER, help="Additional arguments for clang-format")
+	parsed_args = parser.parse_args(args)
+	
+	load_configuration()
+
+	format_file = os.path.join(os.path.dirname(__file__), CONFIGURATION["CLANG_FORMAT_FILE"])
+	format_ignore_file = os.path.join(os.path.dirname(__file__), CONFIGURATION["CLANG_IGNORE_FILE"])
+
+	find_cmd = ["find", parsed_args.path, "-type", "f", "(", "-name", "*.h", "-o", "-name", "*.cpp", "-o", "-name", "*.c", "-o", "-name", "*.hpp", ")"]
+    
+	if os.path.exists(format_ignore_file):
+		with open(format_ignore_file, "r") as f:
+			for pattern in f:
+				pattern = pattern.strip()
+				if not pattern or pattern.startswith("#"):
+					continue
+				find_cmd.extend(["!", "-path", pattern])
+    
+	verbose_flag = ["-verbose"] if parsed_args.v else []
+	find_cmd.extend(["-exec", CONFIGURATION["CLANG_FORMAT_CALL"], "-i", *verbose_flag, f"-style=file:{format_file}", "{}", "+"])
+
+    
+	print("Running:", " ".join(find_cmd))
+    
+	subprocess.run(find_cmd, check=True)
+    
+	print("Formatting complete.")
---
+++ trenchkit/trenchkit.py
@@ -0,0 +1,32 @@
+import importlib
+import os
+import sys
+import argparse
+
+TOOLS_DIR = os.path.join(os.path.dirname(__file__), "tools")
+
+def load_tools():
+    tools = {}
+    dirs = filter(lambda d: os.path.isdir(os.path.join(TOOLS_DIR, d)), os.listdir(TOOLS_DIR))
+    dirs = filter(lambda d: not d.startswith("__"), dirs)
+    for dirname in dirs:
+        module = importlib.import_module(f"trenchkit.tools.{dirname}.main")
+        tools[dirname] = module.run
+    return tools
+
+def main():
+    tools = load_tools()
+
+    parser = argparse.ArgumentParser(description="Tools Program")
+    parser.add_argument("tool", choices=tools.keys(), help="The tool to run")
+    parser.add_argument("args", nargs=argparse.REMAINDER, help="Arguments for the tool")
+
+    args = parser.parse_args()
+
+    if args.tool in tools:
+        tools[args.tool](args.args)
+    else:
+        print(f"Tool '{args.tool}' not found.")
+
+if __name__ == "__main__":
+    main()
